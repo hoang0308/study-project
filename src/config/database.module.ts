81cf0779d3ede13b313e76efe54da9bbad76978f
HoangPH <hoangph@2nf.com.vn>
2023-03-26 20:31:44 +0700

    add_connect_to_db

---
+++ src/config/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get(process.env.POSTGRES_HOST),
+        port: configService.get(process.env.POSTGRES_PORT),
+        database: process.env.POSTGRES_DB,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        synchronize: true,
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
